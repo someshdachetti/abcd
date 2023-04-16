c28539d4ca1d837fa2b4937bcf5a6de6736fb1cf
someshdachetti <dssomesh201@gmail.com>
2023-04-16 06:32:57 +0000

    main

---
+++ public/img/Abcd/index.js
@@ -0,0 +1,13 @@
+import {Component} from 'react'
+import './index.css'
+
+class Abcd extends Component {
+  render() {
+    return (
+      <div>
+        <h>Hello ,user</h>
+      </div>
+    )
+  }
+}
+export default Abcd
---
+++ public/img/Efgh/index.js
@@ -0,0 +1,12 @@
+import './index.css'
+
+const historyItems = props => {
+    const {allHistoryItems}=props
+    const{ id,timeAccessed,logoUrl,title ,domainUrl,}=allHistoryItems
+
+    return {
+        <div >
+            <h1>Somesh</h1>
+        </div>
+    }
+}
